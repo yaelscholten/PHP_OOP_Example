662401d8aa411ecd7d90748400a5fa1c2aeaecd2
yaelscholten <101642932+yaelscholten@users.noreply.github.com>
2024-03-12 13:34:19 +0100

    Update config.php

--- etc/config.php
+++ etc/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'dbuser');
-define('DB_PASSWORD', 'DBpassword21!');
-define('DB_NAME', 'db_php_oop');
+define('DB_USERNAME', 'net24yscholten_yael ');
+define('DB_PASSWORD', 'GN7T!H@B,qR,');
+define('DB_NAME', 'net24yscholten_PHP_OOP_Example');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
